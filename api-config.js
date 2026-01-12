891bfba2b66ce2aaec2c677235ffffe211af35e5
Alex Gonper <alexgonper@gmail.com>
2026-01-12 04:52:37 -0500

    fix: new tunnel URL to fix mobile IP error

--- api-config.js
+++ api-config.js
@@ -1,3 +1,3 @@
 // Permanent backend URL (LocalTunnel for remote access)
-window.API_URL = 'https://lemon-ways-learn.loca.lt/api/v1';
-window.API_BASE_URL = 'https://lemon-ways-learn.loca.lt/api/v1';
+window.API_URL = 'https://moody-readers-thank.loca.lt/api/v1';
+window.API_BASE_URL = 'https://moody-readers-thank.loca.lt/api/v1';
